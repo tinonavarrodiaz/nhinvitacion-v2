53d6a1c2cb2fdfad90b73f474a602a155a48f482
Florentino Navarro Diaz <tino.navarro@hotmail.com>
2025-04-19 15:45:38 -0600

    fix eslint

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -14,8 +14,8 @@ const options = {
   },
 }
 
-let client: MongoClient
 
+let client: MongoClient
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
