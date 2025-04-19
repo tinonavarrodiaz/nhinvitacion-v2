c36de243d550421aa1d5a0b11809c5614a237b76
Florentino Navarro Diaz <tino.navarro@hotmail.com>
2025-04-19 15:31:09 -0600

    add mongodb adapter for google

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,30 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import client from "./libs/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  adapter: MongoDBAdapter(client),
+  providers: [Google({
+    profile(profile) {
+      console.log(profile)
+      return {
+        id: profile.sub,
+        name: profile.name,
+        email: profile.email,
+        image: profile.picture,
+        role: profile.role ?? "user",
+        email_verified: profile.email_verified ? new Date() : undefined
+      }
+    }
+  })],
 })
+/*
+tinonav   : Username mongo atlas
+tIavc5Nlcjc2Mvoh    : password mongo atlas
+
+nhinvita_user   : Username mongo atlas
+kuRUKdZQ5LNk3GF2    : password mongo atlas
+
+mongodb+srv://nhinvita_user:kuRUKdZQ5LNk3GF2@cluster0.mh6ablh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+*/
